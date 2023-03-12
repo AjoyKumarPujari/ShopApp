60eebfc5cc9dcb86d4e9b8eaa292e3f9d5f66d2e
AjoyKumarPujari <ajoykumarpujari@gmail.com>
2023-03-12 14:16:00 -0300

    React app Setup

---
+++ src/components/Item.js
@@ -0,0 +1,15 @@
+function Item(){
+
+    const date = 20;
+    const month ="June";
+    const year = 1998;
+    return(
+       <div className="mfg-date">
+        <span>{date}</span>
+        <span>{month}</span>
+        <span>{year}</span>
+       </div>
+        
+    );
+}
+export default Item;
---
+++ src/components/nirma.js
@@ -0,0 +1,11 @@
+function nirma(){
+
+
+    return(
+       <div className="mfg-date">
+        Nirma
+       </div>
+        
+    );
+}
+export default nirma;
