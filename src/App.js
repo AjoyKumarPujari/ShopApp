ed0b49e2a5a9ef1b8b1c6503c798af3bf74743c0
AjoyKumarPujari <ajoykumarpujari@gmail.com>
2023-03-15 14:28:26 -0300

    add use state

--- src/App.js
+++ src/App.js
@@ -1,48 +1,44 @@
-import Item from './components/Item'
-import ItemDate from './components/ItemDate';
-import Card from './components/Card';
-import  './App.css';
+import React from 'react';
+import NewProduct from './components/NewProduct';
 
-function App() {
-  const response = [
+import Products from './components/Products';
+
+const App = () => {
+  const products = [
     {
-        itemName:"nirma",
-        itemDate: "2",
-        itemMonth: "august",
-        itemYear: "1990"
-
+      id: 'p1',
+      title: 'Nirma',
+      amount: 100,
+      date: new Date(2021, 8, 10),
+    },
+    { 
+      id: 'p2', 
+      title: 'Sirf Excel', 
+      amount: 200, 
+      date: new Date(2021, 2, 2) },
+    {
+      id: 'p3',
+      title: 'Tide',
+      amount: 130,
+      date: new Date(2021, 12, 28),
     },
     {
-      itemName:"surf",
-      itemDate: "2",
-      itemMonth: "august",
-      itemYear: "1990"
+      id: 'p4',
+      title: 'Maggi',
+      amount: 450,
+      date: new Date(2021, 5, 5),
+    },
+  ];
 
-  },
-  {
-    itemName:"555",
-    itemDate: "5",
-    itemMonth: "april",
-    itemYear: "1990"
+  function printProductData(data) {
+    console.log("i am inside APP.js")
+    console.log(data)
+  }
 
-},
-  ]
   return (
-    <div className="App">
-      <Card>
-        <Item name={response[0].itemName} >
-          hello
-        </Item>
-        <ItemDate day={response[0].itemDate} month={response[0].itemMonth} year={response[0].itemYear}></ItemDate>
-
-        <Item name={response[1].itemName} ></Item>
-        <ItemDate day={response[1].itemDate} month={response[1].itemMonth} year={response[1].itemYear}></ItemDate>
-
-        <Item name={response[2].itemName} ></Item>
-        <ItemDate day={response[2].itemDate} month={response[2].itemMonth} year={response[2].itemYear}></ItemDate>
-        <div className="App">Hello Jee</div>
-        </Card>
-      
+    <div>
+      <NewProduct pranay = {printProductData} />
+      <Products items={products} />
     </div>
   );
 }
